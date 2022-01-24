9b9e162f4f80b9984e64880396cdb1cb3a5938cd
yuribertoli <yuribertoli@yahoo.it>
2022-01-24 17:12:28 +0100

    genera js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 //Definisco le costanti 
 const pricePerKm = 0.21;
-const ageAdult = 18;
-const ageElderly = 65;
 const discountUnderAge = 0.2;
 const discountOver65 = 0.4;
 
 //Definisco le variabili iniziali
 let namePassenger = document.getElementById("namePassenger");
 let kmToRun = document.getElementById("kmToRun");
-let agePassenger = document.getElementsByTagName("select");
+let agePassenger = document.getElementById("select");
 
 // Variabili bottoni da premere
 let processData = document.getElementById("process-data");
@@ -23,48 +21,37 @@ let randomCpCode = Math.floor(Math.random() * (max - min) + min);
 let randomCoach = Math.floor(Math.random() * 9) + 1;
 
 //Calcolo prezzo biglietto standard
-let priceTicket = kmToRun * pricePerKm;
-document.getElementById("traveller-name").innerHTML = namePassenger;
-document.getElementById("ticket-type").innerHTML = "Biglietto Standard";
-document.getElementById("coach").innerHTML = randomCoach;
-document.getElementById("CP-code").innerHTML = randomCpCode;
-document.getElementById("price").innerHTML = priceTicket.toFixed(2) + "€";
+let priceTicket = kmToRun.value * pricePerKm;
+
+//Calcolo prezzi biglietti scontati
+let priceDiscounted18 = priceTicket - (priceTicket * discountUnderAge);
+let priceDiscounted65 = priceTicket - (priceTicket * discountOver65);
+
+//Click su bottone "Genera"
+processData.addEventListener("click",
+
+    function() {
+
+        document.getElementById("traveller-name").innerHTML = namePassenger.value;
+        document.getElementById("coach").innerHTML = randomCoach;
+        document.getElementById("CP-code").innerHTML = randomCpCode;
+
+        if (agePassenger.value == "Minorenne") {
+            document.getElementById("price").innerHTML = priceDiscounted18.toFixed(2) + "€";
+            document.getElementById("ticket-type").innerHTML = "Biglietto Under18";
+        } else if (agePassenger.value == "Over65") {
+            document.getElementById("price").innerHTML = priceDiscounted65.toFixed(2) + "€";
+            document.getElementById("ticket-type").innerHTML = "Biglietto Over65";
+        } else {
+            document.getElementById("price").innerHTML = priceTicket.toFixed(2) + "€";
+            document.getElementById("ticket-type").innerHTML = "Biglietto Standard";
+        } 
+
+    }
+
+);
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let priceDiscounted;
-let underAgeSavings = priceTicket * discountUnderAge;
-let elderlySavings = priceTicket * discountOver65;
-
-if (agePassenger < ageAdult){
-    priceDiscounted = priceTicket - (priceTicket * discountUnderAge); 
-    document.getElementById("price").innerHTML = priceDiscounted.toFixed(2);
-} else if (agePassenger >= ageElderly) {
-    priceDiscounted = priceTicket - (priceTicket * discountOver65); 
-    document.getElementById("price").innerHTML = priceDiscounted.toFixed(2);
-}
 
